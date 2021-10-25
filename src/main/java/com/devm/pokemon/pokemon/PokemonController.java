2f15e4fcde3cc34a9ffe1f73fdf35e9f4ab0bc75
marcos <marcos.ant.pereira.jr@gmail.com>
2021-10-25 14:09:27 -0300

    Swagger

--- src/main/java/com/devm/pokemon/pokemon/PokemonController.java
+++ src/main/java/com/devm/pokemon/pokemon/PokemonController.java
@@ -7,19 +7,22 @@ import com.devm.pokemon.feign.PokemonServiceAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import lombok.AllArgsConstructor;
 
 
 
 
-
+@Api(value = "Pokemon", description = "Operations about pokemons")
 @RestController
 @RequestMapping(path = "/pokemon",produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor(onConstructor = @__(@Autowired))
@@ -28,6 +31,15 @@ public class PokemonController {
     private final PokemonRepository repository;
     private final PokemonServiceAPI pokemonServiceAPI;
 
+    @ApiOperation(
+        tags = "Pokemon",
+        value = "Get all Pokemons",
+        notes = "Pokemons saved in Database",
+        response = Pokemon.class,
+        responseContainer = "List",
+        nickname = "getAllPokemon"
+    ) 
+    @ApiResponse(code = 200, message = "Pokemon saved")
     @GetMapping()
     public ResponseEntity<?> getMethodName() {
         return ResponseEntity.ok().body(repository.findAll());
